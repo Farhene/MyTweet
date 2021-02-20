edd1f13bf7e1572cc09fa95ef3bfa3d5a40ef6ad
Farhene Sultana <farhenesultana@gmail.com>
2021-02-19 22:51:36 -0500

    Finished Unit 4 Assignment!

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -21,14 +21,29 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweeting: \(error)")
+        })
         
         
     }
     
-    var favorited:Bool = false
-    var tweetId:Int = -1
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
     
     
     func setFavorite(_ isFavorited:Bool){
