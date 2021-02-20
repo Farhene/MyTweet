c5eb17315ceff92a11a0977c0da5d2bd808ebf58
Farhene Sultana <farhenesultana@gmail.com>
2021-02-19 20:31:28 -0500

    Enabled logout function.

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Farhene Sultana on 2/19/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func loadTweets(){
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetsContent.text = "Something else"
+        
+        
+        return cell
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    
+}
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,14 +16,27 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            //we ask here to run segue right away
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+        
+        
+    }
     
     @IBAction func onLoginButton(_ sender: Any) {
         
         let myURL = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: myURL, success: {
+            
+            //we want to write a note to memory that user is still logged in
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             //we will do perform segue.
             self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
         }, failure: { (Error) in
             print("Could not log in!")
         })
